1d731c21d778e68fcbb45813ef3f5efa3006d333
Duncan Vriend <vriendduncan@gmail.com>
2026-04-27 13:46:25 +0200

    rename: `write_buf` to `output_buf`

--- src/main.zig
+++ src/main.zig
@@ -5,11 +5,10 @@ const chess = @import("chess");
 const stdout = std.fs.File.stdout();
 const stdin = std.fs.File.stdin();
 
-var write_buf: [64]u8 = undefined;
+var output_buf: [64]u8 = undefined;
 var input_buf: [64]u8 = undefined;
 pub fn main() !void {
     std.log.info("Tile size: {d}", .{@sizeOf(chess.Tile)});
-    // var console_out = stdout.writer(write_buf);
-    // while () {}
+    // var console_out = stdout.writer(output_buf);
     // console_out.interface.print(comptime fmt: []const u8, args: anytype)
 }
