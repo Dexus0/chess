4bdcf4c93a3ac1b88359598c3a780270a52930d6
Duncan Vriend <vriendduncan@gmail.com>
2026-04-27 15:09:47 +0200

    more refactors and such

--- src/main.zig
+++ src/main.zig
@@ -5,10 +5,17 @@ const chess = @import("chess");
 const stdout = std.Io.File.stdout();
 const stdin = std.Io.File.stdin();
 
-var output_buf: [64]u8 = undefined;
-var input_buf: [64]u8 = undefined;
-pub fn main() !void {
-    std.log.info("Tile size: {d}", .{@sizeOf(chess.Tile)});
-    // var console_out = stdout.writer(output_buf);
-    // console_out.interface.print(comptime fmt: []const u8, args: anytype)
+var output_buf: [64]u8 align(64) = undefined;
+var input_buf: [64]u8 align(64) = undefined;
+
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
+    var console_out = stdout.writer(io, output_buf);
+    var console_in = stdin.reader(io, input_buf);
+    std.debug.print("Tile size: {d}", .{@sizeOf(chess.Tile)});
+}
+
+fn print_board(writer: std.Io.Writer, board: chess.Board) !void {
+    _ = board; // autofix
 }
